3398da1764114674243915d5bf2926cf9a6b34aa
Iryna Burlaka <i.am.blinova@gmail.com>
2024-08-12 11:52:34 +0200

    connnect of mongoos

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }
+}
 
 const app = express();
 
