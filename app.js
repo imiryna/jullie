9ac811810de9c213aab6004683f2652d023ea7bc
Iryna Burlaka <i.am.blinova@gmail.com>
2024-08-29 10:16:11 +0200

    controllers into new file

--- app.js
+++ app.js
@@ -31,57 +31,10 @@ app.use("/api/todo", TodoRouter);
 
 //create REST API
 
-app.post("/todos", async (req, res) => {
-  try {
-    const { name, description, dueDate, priority } = req.body;
+app.post("/todos");
+app.get("/todos");
 
-    // req.body validation
-    if (!name || !description || !dueDate || !priority) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const newTodo = {
-      name,
-      description,
-      dueDate,
-      priority,
-    };
-
-    //save newTodo to DB
-    const saveTodo = TodoModel.create(newTodo);
-
-    res.status(201).json(saveTodo);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-app.get("/todos", async (req, res) => {
-  try {
-    const allTodo = await TodoModel.find();
-
-    res.status(200).json(allTodo);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-app.get("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params.id;
-    const allTodo = await TodoModel.find();
-
-    const todo = allTodo.find((item) => {
-      item.id === id;
-    });
-    res.status(200).json({
-      message: "Success",
-      todo,
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+app.get("/todos/:id");
 
 //app.patch();
 
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,57 @@
-// import { model } from "mongoose";
-//import { getTodoList, createTodo } from "../services/todoService.js";
+import { model } from "mongoose";
+// import { getTodoList, createTodo } from "../services/todoService.js";
 
-// const getAllTodo = async (req, res, next) => {
-//   try {
-//     const result = await getTodoList(req, res);
-//     res.status(200).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+export const getAllTodo = async (req, res) => {
+  try {
+    const allTodo = await TodoModel.find();
 
-// const createNewTodo = async (req, res, next) => {
-//   try {
-//     const result = await createTodo(req, res);
-//     res.status(200).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+    res.status(200).json(allTodo);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getTodoBiId = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const allTodo = await TodoModel.find();
+
+    const todo = allTodo.find((item) => {
+      item.id === id;
+    });
+    res.status(200).json({
+      message: "Success",
+      todo,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const createNewTodo = async (req, res) => {
+  try {
+    const { name, description, dueDate, priority } = req.body;
+
+    // req.body validation
+    if (!name || !description || !dueDate || !priority) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newTodo = {
+      name,
+      description,
+      dueDate,
+      priority,
+    };
+
+    //save newTodo to DB
+    const saveTodo = TodoModel.create(newTodo);
+
+    res.status(201).json(saveTodo);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 //export { getAllTodo };
 // module.exports = getAllTodo;
