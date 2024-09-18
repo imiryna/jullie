30f9b51e89e91afd607f5bdc2a1f9b32110631b1
Iryna Burlaka <i.am.blinova@gmail.com>
2024-09-18 11:57:17 +0200

    joi validator

--- utils/userValidator.js
+++ utils/userValidator.js
@@ -1,1 +1,16 @@
 import Joi from "joi";
+
+export const createUserDataValidator = (data) => {
+  Joi.object().keys(
+    {
+      username: Joi.string().alphanum().min(3).max(30).required(),
+      name: Joi.string.min(3).max(24).required(),
+      email: Joi.string().email(),
+      password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
+      repeat_password: Joi.ref("password"),
+
+      access_token: [Joi.string(), Joi.number()],
+    }.validation(date)
+  );
+};
