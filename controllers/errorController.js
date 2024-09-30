50d919f815c23cfbadf8976c7d904e33961f4ed7
Iryna Burlaka <i.am.blinova@gmail.com>
2024-09-30 13:41:31 +0200

    global server error to start

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,20 @@
+import { serverConfig } from "../config/serverConfig,js";
+
 export const globalErrorHandler = (err, req, res, next) => {
-  res.status(err.status ?? 500).json({ message: err.message });
+  let msg;
+  let stack;
+  let error;
+
+  if (serverConfig) {
+    msg = err.message;
+    stack = err.stack;
+    error = err;
+  } else {
+    msg = err.status === 500 ? "Internal server error" : err.message;
+  }
+  res.status(err.status ?? 500).json({
+    msg,
+    stack,
+    err,
+  });
 };
