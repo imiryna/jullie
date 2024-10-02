9a49f55251a7cf71197987e8fcc8f6095f91f949
Iryna Burlaka <i.am.blinova@gmail.com>
2024-10-02 12:23:01 +0200

    global err change

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,20 +1,13 @@
 import { serverConfig } from "../config/serverConfig,js";
 
 export const globalErrorHandler = (err, req, res, next) => {
-  let msg;
-  let stack;
-  let error;
-
-  if (serverConfig) {
-    msg = err.message;
-    stack = err.stack;
-    error = err;
-  } else {
-    msg = err.status === 500 ? "Internal server error" : err.message;
+  if (serverConfig === "prodaction") {
+    return res.status(err.status ?? 500).json({
+      msg: !err.status || err.status === 500 ? "Internal server error" : err.message,
+    });
   }
   res.status(err.status ?? 500).json({
-    msg,
-    stack,
-    err,
+    msg: err.message,
+    stack: err.stack,
   });
 };
