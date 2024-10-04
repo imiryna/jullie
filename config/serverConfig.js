a385c281ced4cf694150d288e3ad98065fead917
Iryna Burlaka <i.am.blinova@gmail.com>
2024-10-04 11:59:43 +0200

    .enw and config

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,3 +1,6 @@
 export const serverConfig = {
-  environment: process.env.NODE_ENV,
+  mongoUrl: process.env.MONGO_URL ?? "mongodb/localhost:27017",
+  environment: process.env.NODE_ENV ?? "development",
+  jwtSecret: process.env.JWT_SECRET ?? "secret-phrase",
+  jwtExpare: process.env.JWT_EXPARE ?? "1d",
 };
