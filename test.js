89422490b63a154520b65c3378433b1108d9bdcf
Iryna Burlaka <i.am.blinova@gmail.com>
2024-11-12 15:23:24 +0100

    fix

--- test.js
+++ test.js
@@ -106,7 +106,25 @@ function solution(N) {
 }
 console.log(solution(1041));
 
-// function solution(A) {
+// task about A non-empty array A consisting of N integers is given.
+
+// A permutation is a sequence containing each element from 1 to N once, and only once.
+
+// For example, array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+//     A[3] = 2
+// is a permutation, but array A such that:
+
+//     A[0] = 4
+//     A[1] = 1
+//     A[2] = 3
+// is not a permutation, because value 2 is missing.
+
+// The goal is to check whether array A is a permutation
+//function solution(A) {
 //     // Implement your solution here
 //     const n = A.length;
 //     const arr = new Set(A);
